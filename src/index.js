a348dd698c4fc151dacdff6496016280ef8f9a9a
Oxana Shvartsman <displaygreat@gmail.com>
2021-04-03 22:15:39 +0300

    create: containers, components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App.js';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import store from './store';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </Provider>
+, document.getElementById('root'));
+// registerServiceWorker();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+
+// ReactDOM.render(
+//   <App />, document.getElementById('root')
+// );
