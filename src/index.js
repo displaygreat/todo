216df694949ce0b95009f7dcb7309afab553c198
Oxana Shvartsman <displaygreat@gmail.com>
2021-05-07 22:55:28 +0300

    finished

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.js';
-import './index.css';
-// import registerServiceWorker from './registerServiceWorker';
-import { Provider } from 'react-redux';
-import store from './store';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.js";
+import "./index.css";
+import { Provider } from "react-redux";
+import store from "./store";
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
-  </Provider>
-, document.getElementById('root'));
-// registerServiceWorker();
+  </Provider>,
+  document.getElementById("root")
+);
